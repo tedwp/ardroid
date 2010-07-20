a527e0c27fb18ce5dca7bc5a0ebadccc263e3921
lander16 <lander16@aaf85c3b-0f68-5df5-264b-b94e4bc847cf>
2010-07-20 03:33:24 +0000

    Added class for HTTPConnection Added methods for determining a POI x position Extracted some common behavior to methods Added a synchronized ArrayList to store POIs Did some refactoring

---
+++ ARDroid/src/mx/unam/fciencias/ardroid/app/HttpConnection.java
@@ -0,0 +1,79 @@
+package mx.unam.fciencias.ardroid.app;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.util.Log;
+
+public class HttpConnection {
+
+	public static String sendPost(String address, String message) {
+		BufferedReader in = null;
+		try {
+			HttpClient client = new DefaultHttpClient();
+			HttpPost request = new HttpPost(address);
+			StringEntity stringEntity = new StringEntity(message);
+			request.setEntity(stringEntity);
+			HttpResponse response = client.execute(request);
+			in = new BufferedReader(new InputStreamReader(response.getEntity()
+					.getContent()));
+			String result = in.readLine();
+			return result;
+
+		} catch (Exception e) {
+			Log.d("login", "excepcion en la conexión post:\n" + e);
+			e.printStackTrace();
+			return null;
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public static String sendGet(String address,
+			String[] headerElements) {
+		BufferedReader in = null;
+		try {
+			HttpClient client = new DefaultHttpClient();
+			HttpGet request = new HttpGet(address);
+			HttpResponse response = client.execute(request);
+			in = new BufferedReader(new InputStreamReader(response.getEntity()
+					.getContent()));
+			StringBuffer sb = new StringBuffer("");
+			String line = "";
+			String NL = System.getProperty("line.separator");
+			while ((line = in.readLine()) != null) {
+				sb.append(line + NL);
+			}
+			in.close();
+			String result = sb.toString();
+			return result;
+		} catch (Exception e) {
+			Log.d("login", "excepcion en la conexion get:\n" + e);
+			e.printStackTrace();
+			return null;
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+}
