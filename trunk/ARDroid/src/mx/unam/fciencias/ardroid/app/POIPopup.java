ad971d5d42b8ae840d13d2842467a490f3d7866d
lander16 <lander16@aaf85c3b-0f68-5df5-264b-b94e4bc847cf>
2010-07-25 22:07:15 +0000

    -Popup finished: Displays progressbar, title and distance -Popup enter and exit slide animations -Touch events handled by each POI

--- trunk/ARDroid/src/mx/unam/fciencias/ardroid/app/POIPopup.java
+++ trunk/ARDroid/src/mx/unam/fciencias/ardroid/app/POIPopup.java
@@ -1,8 +1,12 @@
 package mx.unam.fciencias.ardroid.app;
 
-import android.content.Context;
+import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.widget.PopupWindow;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.*;
 
 /**
  * Creado por: Sebasti‡n Garc’a Anderman
@@ -11,8 +15,82 @@ import android.widget.PopupWindow;
  */
 public class POIPopup extends PopupWindow {
 
-    public POIPopup() {
-        super(((LayoutInflater) Main.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.popup, null, false));
-        
+    WebView wv;
+    private static boolean shown = false;
+    private TextView poiTitle;
+    private TextView poiDistance;
+    private ProgressBar progressBar;
+    private RelativeLayout progressBarLayout;
+
+
+    public POIPopup(int screenWidth, int screenHeight) {
+        super(Main.context);
+        View v = LayoutInflater.from(Main.context).inflate(R.layout.popup, null);
+        this.setWidth(screenWidth - (screenWidth / 20));
+        this.setHeight(screenHeight - (screenHeight / 40));
+        this.setAnimationStyle(R.style.Animation_Popup);
+        this.setFocusable(true);
+        this.update();
+        this.setContentView(v);
+        ImageButton b = (ImageButton) v.findViewById(R.id.closebutton);
+        poiTitle = (TextView) v.findViewById(R.id.title);
+        poiDistance = (TextView) v.findViewById(R.id.distance);
+        progressBar = (ProgressBar) v.findViewById(R.id.progressbar_popup);
+        progressBarLayout = (RelativeLayout) v.findViewById(R.id.progressbar_layout);
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+
+        wv = (WebView) v.findViewById(R.id.webView);
+        wv.getSettings().setJavaScriptEnabled(true);
+
+        wv.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                /*
+                Activities and WebViews measure progress with different scales.
+                The progress meter will automatically disappear when we reach 100%
+                */
+                progressBar.setProgress(progress);
+                if(progress == 100){
+                    progressBarLayout.setVisibility(ProgressBar.GONE);
+                }
+            }
+        });
+        wv.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(Main.context, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public void showAtLocation(View parent, int gravity, int x, int y) {
+        if (!shown) {
+            super.showAtLocation(parent, gravity, x, y);
+            shown = true;
+        }
+    }
+
+    @Override
+    public void dismiss() {
+        shown = false;
+        super.dismiss();
+    }
+
+    public void show(POI poi) {
+        poiTitle.setText(poi.getName());
+        poiDistance.setText(Main.context.getText(R.string.distance_text_popup) + " " + Float.toString(poi.getDistance()) + "m");
+        wv.loadData("", "text/html", "utf-8");
+        if (poi.getInfoUrl() != null) {
+            progressBarLayout.setVisibility(ProgressBar.VISIBLE);
+            progressBar.setProgress(0);
+            wv.loadUrl(poi.getInfoUrl());
+        } else {
+            wv.loadData("<h2>"+Main.context.getString(R.string.no_extra_data_popup)+"</h2>", "text/html", "utf-8");
+        }
+        showAtLocation(ARLayer.arView, Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM, 0, 0);
     }
 }
--- trunk/ARDroid/src/mx/unam/fciencias/ardroid/app/TestPOIDrawing.java
+++ trunk/ARDroid/src/mx/unam/fciencias/ardroid/app/TestPOIDrawing.java
@@ -9,39 +9,39 @@ public class TestPOIDrawing {
 		Location location = new Location("test");
 		location.setLatitude(19.322339);
 		location.setLongitude(-103.234767);
-		POI p1 = new POI(location, null, "Oeste");
+		POI p1 = new POI(location, null, "Oeste", "http://es.wikipedia.org/wiki/Oeste");
 
 		Location location2 = new Location("test2");
 		location2.setLatitude(27.322967);
 		location2.setLongitude(-99.234767);
-		POI p2 = new POI(location2, null, "Norte");
+		POI p2 = new POI(location2, null, "Norte", "http://es.wikipedia.org/wiki/Norte");
 
 		Location location3 = new Location("test3");
 		location3.setLatitude(19.322339);
 		location3.setLongitude(-96.534767);
-		POI p3 = new POI(location3, null, "Este");
+		POI p3 = new POI(location3, null, "Este", "http://es.wikipedia.org/wiki/Este");
 
 		Location location4 = new Location("test4");
 		location4.setLatitude(19.022967);
 		location4.setLongitude(-99.234767);
 		Log.d("gps", "Location 4: "+location4.getLatitude()+", "+location4.getLongitude());
-		POI p4 = new POI(location4, null, "Sur");
+		POI p4 = new POI(location4, null, "Sur", "http://es.wikipedia.org/wiki/Sur");
 
 		Location location5 = new Location("test");
 		location5.setLatitude(19.322339);
 		location5.setLongitude(-99.235229);
-		POI p5 = new POI(location5, null, "Casa de Seliks");
+		POI p5 = new POI(location5, null, "Casa de Seliks", "http://www.google.com");
 
 		Location location6 = new Location("test");
 		location6.setLatitude(19.322967);
 		location6.setLongitude(-99.234767);
-		POI p6 = new POI(location6, null, "Casa 10");
+		POI p6 = new POI(location6, null, "Casa 10", "http://www.google.com");
 
 		Location location7 = new Location("test");
 		location7.setLatitude(19.321592);
 		location7.setLongitude(-99.253535);
 		location7.setAltitude(2754);
-		POI p7 = new POI(location7, null, "Cerro del Judío");
+		POI p7 = new POI(location7, null, "Cerro del Judío", "http://www.google.com");
 
 
 //		ARLayer.poiList.add(p1);
